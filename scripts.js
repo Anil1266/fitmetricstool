f5fddbf1dfb8be8e6f154a698b0f39b0d17b6e19
Anil1266 <141404291+Anil1266@users.noreply.github.com>
2026-04-01 11:45:05 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,99 +1,36 @@
 function calculateBMI(){
-
-let weight=document.getElementById("weight").value
-let height=document.getElementById("height").value
-
-height=height/100
-
-let bmi=weight/(height*height)
-
-bmi=bmi.toFixed(1)
-
-let category=""
-
-if(bmi<18.5){
-category="Underweight"
+let w=document.getElementById("weight").value
+let h=document.getElementById("height").value/100
+let bmi=(w/(h*h)).toFixed(1)
+document.getElementById("result").innerHTML="BMI: "+bmi
 }
-else if(bmi<25){
-category="Normal weight"
-}
-else if(bmi<30){
-category="Overweight"
-}
-else{
-category="Obese"
-}
-
-document.getElementById("result").innerHTML=
-"Your BMI: "+bmi+" ("+category+")"
-
-}
-
-
-
-function calculateWater(){
-
-let weight=document.getElementById("weight").value
-
-let water=weight*35
-
-water=water/1000
-
-water=water.toFixed(2)
-
-document.getElementById("result").innerHTML=
-"You should drink about "+water+" liters of water daily"
-
-}
-
-
-
-function calculateIdealWeight(){
-
-let height=document.getElementById("height").value
-
-let weight=(height-100)*0.9
-
-weight=weight.toFixed(1)
-
-document.getElementById("result").innerHTML=
-"Your ideal weight is about "+weight+" kg"
-
-}
-
-
 
 function calculateBMR(){
-
-let weight=document.getElementById("weight").value
-let height=document.getElementById("height").value
-let age=document.getElementById("age").value
-
-let bmr=(10*weight)+(6.25*height)-(5*age)+5
-
-bmr=bmr.toFixed(0)
-
-document.getElementById("result").innerHTML=
-"Your BMR is "+bmr+" calories/day"
-
+let w=document.getElementById("weight").value
+let h=document.getElementById("height").value
+let a=document.getElementById("age").value
+let bmr=(10*w)+(6.25*h)-(5*a)+5
+document.getElementById("result").innerHTML="BMR: "+bmr
 }
 
-
-
 function calculateTDEE(){
+let w=document.getElementById("weight").value
+let h=document.getElementById("height").value
+let a=document.getElementById("age").value
+let act=document.getElementById("activity").value
+let bmr=(10*w)+(6.25*h)-(5*a)+5
+let tdee=bmr*act
+document.getElementById("result").innerHTML="TDEE: "+tdee
+}
 
-let weight=document.getElementById("weight").value
-let height=document.getElementById("height").value
-let age=document.getElementById("age").value
-let activity=document.getElementById("activity").value
+function calculateWater(){
+let w=document.getElementById("weight").value
+let water=(w*35)/1000
+document.getElementById("result").innerHTML="Water: "+water+" L/day"
+}
 
-let bmr=(10*weight)+(6.25*height)-(5*age)+5
-
-let tdee=bmr*activity
-
-tdee=tdee.toFixed(0)
-
-document.getElementById("result").innerHTML=
-"Your TDEE is "+tdee+" calories/day"
-
+function calculateIdealWeight(){
+let h=document.getElementById("height").value
+let weight=(h-100)*0.9
+document.getElementById("result").innerHTML="Ideal Weight: "+weight+" kg"
 }
